d82a1e7516a3ea3771f9979b9c18b8fc696d75e2
manfye <manfye@gmail.com>
2022-02-16 14:51:42 +0800

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,34 @@
-from collections import namedtuple
-import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import datetime as datetime
 
-"""
-# Welcome to Streamlit!
+st.write(datetime.datetime.strptime("2013-1-25", '%Y-%m-%d'))
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    # Can be used wherever a "file-like" object is accepted:
+    df_dateVaccine = pd.read_csv(uploaded_file)
+    st.write(df_dateVaccine)
+    #  df_dateVaccine = pd.read_csv("Aefi_serious.csv")
+    x = datetime.datetime.now()
+    df_dateVaccine["date"] = df_dateVaccine.apply(lambda x: datetime.datetime.strptime(x["date"], '%d/%m/%Y'), axis=1)
+    # except:
+    #     df_dateVaccine["date"] = df_dateVaccine.apply(lambda x: datetime.datetime.strptime(x["date"], '%d-%m-%y'), axis=1)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    # df_dateVaccine.to_csv("generated/"+"aefi_serious_"+str(x.strftime("%Y-%m-%d"))+".csv", index= False)
+    def convert_df(df):
+        return df.to_csv(index=False).encode('utf-8')
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    csv = convert_df(df_dateVaccine)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    st.download_button(
+    "Press to Download",
+    csv,
+    "file.csv",
+    "text/csv",
+    key='download-csv'
+    )
